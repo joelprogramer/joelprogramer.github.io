396a8310bfcf8a965974d4ecbffd6ab86f47a17e
Joel Swartling <josw1204@storsjogymnasiet.se>
2023-04-06 11:39:22 +0200

    klar

--- gissa/gissa.js
+++ gissa/gissa.js
@@ -11,9 +11,29 @@ let gissa_int = Number(textfelt_gissa);
 let vinn = false;
 let p_svar = document.getElementById("p_svar");
 
+if (antalgissningar < 3) {
+    if (slumptal === gissa_int){
+        console.log("RÄTT! antalgissningar= $(antalgissningar)");
+        vinn = true;
+        p_svar.innerHTML= "you're a winner";
+        antalgissningar = 3;
 
+    }
+    else {
+        console.log('FEL! antalgissningar= $(antalgissningar)');
 
+        if (antalgissningar === 2)
+        p_svar.innerHTML='LOOSER!';
+        else
+        //p_svar.innerHTML= (textfelt_gissa)+ ' är FEL! gissning nr ' +$(antalgissningar+1)+ ' av 3';
+        p_svar.innerHTML=(`${textfelt_gissa} är FEL! gissning nr ${antalgissningar+1} av 3 `)
 
-console.log("textfelt=" + textfelt_gissa)
-
+        document.getElementById("gissa").value="";
+        document.getElementById("gissa").focus();
+    }
+    antalgissningar++;
+}
+else{
+    p_svar.innerHTML='LOOSER!';
+}
 }
