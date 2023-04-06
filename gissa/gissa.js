52ab9ebb5302e8561d41e4de3e41d8d789f49105
Joel Swartling <josw1204@storsjogymnasiet.se>
2023-04-06 10:53:09 +0200

    js nästan klar

---
+++ gissa/gissa.js
@@ -0,0 +1,19 @@
+const slumptal = Math.floor(Math.random() * 9) + 1;
+let antalgissningar = 0;
+
+console.log("slumptalet: " + slumptal)
+
+function gissaTal(){
+console.log("click")
+
+let textfelt_gissa =document.getElementById("gissa").value;
+let gissa_int = Number(textfelt_gissa);
+let vinn = false;
+let p_svar = document.getElementById("p_svar");
+
+
+
+
+console.log("textfelt=" + textfelt_gissa)
+
+}
