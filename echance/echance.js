5dc7d6ffad8011e0736d8bd756ebb6031177c0bd
Joel Swartling <josw1204@storsjogymnasiet.se>
2023-05-11 10:13:12 +0200

    halva echance klar

---
+++ echance/echance.js
@@ -0,0 +1,11 @@
+const input_pris = document.getElementById("pris");
+const input_inbet = document.getElementById("inbet");
+
+const svardiv = document.getElementById("svar_div")
+
+const kalkyleraknapp = document.getElementById("kalk_knapp")
+kalkyleraknapp.addEventListener("click", kalkyleraClick)
+
+function kalkyleraClick(e){
+    console.log(`klick`)
+}
