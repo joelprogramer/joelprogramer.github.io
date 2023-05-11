d228c990c87d9b44b0a6cedaefc34d13e563c0a4
Joel Swartling <josw1204@storsjogymnasiet.se>
2023-05-11 11:44:39 +0200

    nästan klar

--- echance/echance.js
+++ echance/echance.js
@@ -1,3 +1,42 @@
+class Pengar{
+    constructor(antal, pengasort){
+        this.antal = antal
+        this.pengasort = pengasort
+    }
+
+}
+
+
+/**
+ * exChance
+ * in: belop som ska växlas, Svalör på sedel
+ * out: antal sedlar eller mynt
+ */
+function exChance(belop, sedel){
+    return Math.floor(belop / sedel)
+}
+
+/**
+ * getExChangeArray()
+ * inbetalt belop
+ * out: array innehåller antal mynt i varje valör
+ */
+
+function getExChangeArray(inbetalning, priset){
+
+    let antal_mynt = 0
+    let pengar_tilbaka
+
+    sedlar_mynt_array = []
+
+    //500---------------------------------
+    pengar_tilbaka = inbetalning - priset
+antal_mynt = exChance(pengar_tilbaka, 500)
+
+console.log(`antal= ${antal_mynt}`)
+
+}
+
 const input_pris = document.getElementById("pris");
 const input_inbet = document.getElementById("inbet");
 
@@ -7,5 +46,10 @@ const kalkyleraknapp = document.getElementById("kalk_knapp")
 kalkyleraknapp.addEventListener("click", kalkyleraClick)
 
 function kalkyleraClick(e){
-    console.log(`klick`)
+
+    let t_pris = parseInt(input_pris.value)
+    let t_inbet = parseInt(input_inbet.value)
+
+    getExChangeArray(t_inbet, t_pris)
+    //console.log(`klick`)
 }
